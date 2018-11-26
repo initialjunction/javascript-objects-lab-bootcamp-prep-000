a1ef6c95de81ef4a667faca396e6e7fb35bc28fa
Joanna Tomassoni <jctomassoni@gmail.com>
2018-11-26 23:04:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newRecipe = Object.assign({}, object, { [key]: value });
   return newRecipe;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.key
+}
