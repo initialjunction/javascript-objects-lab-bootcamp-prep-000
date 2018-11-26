b7320d876861ce91158084401de199f0a8cf5250
Joanna Tomassoni <jctomassoni@gmail.com>
2018-11-26 22:57:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = { flour: 1 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newRecipe = Object.assign({}, object, key, value);
+  var newRecipe = Object.assign({}, object, key value);
   return newRecipe;
 }
